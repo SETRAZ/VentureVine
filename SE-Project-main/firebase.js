47675736e24ce38eba8d35664198f5be5ed86835
Sai Hemanth <86901650+SETRAZ@users.noreply.github.com>
2023-05-16 00:30:10 +0530

    Update firebase.js

--- SE-Project-main/firebase.js
+++ SE-Project-main/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxFpBSosfVK3NeHp-GjGgiScxBjXDyGoo",
+  apiKey: "apikey",
   authDomain: "venturevine-624ef.firebaseapp.com",
   projectId: "venturevine-624ef",
   storageBucket: "venturevine-624ef.appspot.com",
-  messagingSenderId: "967756362000",
-  appId: "1:967756362000:web:ac4a32c2a6006aa9cbf595",
-  measurementId: "G-YZQ3CL01JL"
+  messagingSenderId: "messageid",
+  appId: "appid",
+  measurementId: "m-id"
 };
 
 const app = initializeApp(firebaseConfig);
